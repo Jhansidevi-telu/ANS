bc3151129a9f9d46f4838d2f98c4af1b71c552ae
Jhansidevi-telu <jhansitelu@gmail.com>
2024-10-18 00:20:47 +0530

    changes in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Gallery from "./gallery/Gallery";
-import Header from "./Header/Header";
 import Home from "./Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./Header/Footer/Footer";
